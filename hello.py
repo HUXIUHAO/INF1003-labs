9c2b21a9e8917bcbbd909c7870ff60e741178112
Hu Xiuhao <2604283@sit.singaporetech.edu.sg>
2026-09-06 16:43:13 +0800

    second post

---
+++ hello.py
@@ -0,0 +1,52 @@
+print("=================")
+print("welcome here")
+print('my first blog!')
+print("=================")
+
+
+
+username = "cool_creator"
+bio = "fun_blogger"
+follower = 100
+#sorry for not using capital letters :p
+
+print("username:",username)
+print("bio:",bio)
+print("follower:",follower)
+
+follower +=50
+print("day1:",follower)
+
+follower -=20
+print("day2:",follower)
+
+follower -=10
+print("day3:",follower)
+
+username = input("enter username:")
+age = input("enter age:")
+category = input("enter category")
+
+print("\nInstagram profile")
+print("====================")
+print("username:",username)
+print("age",age)
+print("category",category)
+
+username = input("enter username:")
+age = int(input("enter age"))
+category = input("enter category")
+
+print("\nInstagram profile")
+print("====================")
+print("username:",username)
+print("age",age)
+print("category",category)
+
+if age>40 and category == "fun":
+ print("you are old what is fun for you??")
+#yet sometimes old people do live a joyful life
+#hello.py
+
+
+
