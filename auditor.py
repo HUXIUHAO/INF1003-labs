97c21a3224b94a3c5ed650c4e10e1b1fe8089220
Hu Xiuhao <2604283@sit.singaporetech.edu.sg>
2026-09-11 11:20:14 +0800

    version 2

---
+++ auditor.py
@@ -0,0 +1,16 @@
+stock_quantity = 0
+walk_circle = 0
+for walk_circle in range(111111):
+ stock_quantity_add = int(input("enter a stock quantity:"))
+ stock_quantity + int(stock_quantity_add)
+ stock_quantity_add = 0
+ print("total inventory:",stock_quantity)
+ if stock_quantity < 0:
+  print("ERROR:the quantity is negative")
+ walk_circle + 1
+ if stock_quantity == "stop":
+  break
+
+
+ 
+
