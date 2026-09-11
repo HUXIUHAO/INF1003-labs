ab577ef67e468f979973ec3a7c0a663e4d720ef3
Hu Xiuhao <2604283@sit.singaporetech.edu.sg>
2026-09-11 12:03:41 +0800

    ultimate version

--- auditor.py
+++ auditor.py
@@ -4,11 +4,19 @@ for walk_circle in range(111111):
  user_input = input("enter a stock quantity:")
  if user_input == "stop":
   break
+ if user_input.isdigit() == False:
+  print("ERROR:the quantity is not a number")
+  continue
+ stock_quantity_add = int(user_input)
+ if stock_quantity_add < 0:
+   print("ERROR:the quantity is negative")
+   continue
  stock_quantity_add = int(user_input)
  stock_quantity += stock_quantity_add
  print("total inventory:",stock_quantity)
- if stock_quantity < 0:
-  print("ERROR:the quantity is negative")
+ walk_circle += 1
+print("final inventory:",stock_quantity)
+
 
 
  
