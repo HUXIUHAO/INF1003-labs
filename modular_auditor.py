0a815d0b53b31f381fb15992b4e146b8a79b90f8
Hu Xiuhao <2604283@sit.singaporetech.edu.sg>
2026-09-18 11:21:45 +0800

    first version

---
+++ modular_auditor.py
@@ -0,0 +1,40 @@
+def calculate_tax(original_price): 
+    return original_price * (1+0.10)
+def process_delivery(current_total, new_value):
+    return current_total + new_value 
+def get_valid_input(prompt):
+    while True:
+        user_input = input(prompt)
+        if user_input == "stop":
+            return None
+        if not user_input.isdigit():
+            print("ERROR: the quantity is not a number")
+            failed_attempts +=1
+            continue
+        stock_quantity_add = int(user_input)
+        if stock_quantity_add < 0:
+            print("ERROR: the quantity is negative")
+            failed_attempts +=1
+            continue
+        return stock_quantity_add
+def generate_report(total_units,failed_attempts):
+   print("Total units processed:", total_units)
+   print("Failed attempts:", failed_attempts)    
+current_total = 0
+stock_quantity = 0
+walk_circle = 0
+failed_attempts = 0
+for walk_circle in range(111111):
+ user_input = get_valid_input("enter a stock quantity:")
+ if user_input is None:
+     break
+ stock_quantity_add = int(user_input)
+ stock_quantity += stock_quantity_add
+ print("total inventory:",stock_quantity)
+ walk_circle += 1
+print("final inventory:",stock_quantity)
+generate_report(walk_circle, failed_attempts)
+
+
+
+ 
